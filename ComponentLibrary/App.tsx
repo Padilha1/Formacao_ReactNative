57b0b3e3d694baae51030939a0dd41ba28ce4623
Padilha1 <padilha.matheus@hotmail.com>
2024-12-19 22:45:16 -0300

    Component Library - Ignite Gym

---
+++ ComponentLibrary/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar, Text, View } from "react-native";
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Home</Text> : null}
+    </View>
+  );
+}
